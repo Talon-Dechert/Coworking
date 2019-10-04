13e079e1098e431260d59ee0c04a4258d11249f2
Talon-Dechert <tdechert@live.com>
2019-10-03 20:55:57 -0700

    More updates to layout and index

--- js/js.js
+++ js/js.js
@@ -1,11 +1,22 @@
+let status = 0;
+
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
+  status = 1;
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
+  status = 0;
+}
+
+function alternate() {
+  if (status === 0) {
+    openNav();
+  }
+  else closeNav();
 }
