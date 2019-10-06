205098f6bc2cc715b2a501b3432088c3def0b614
Talon-Dechert <tdechert@live.com>
2019-10-06 16:19:48 -0700

    Trying to add mobile responsiveness to sidebar

--- js/index.js
+++ js/index.js
@@ -1,17 +1,28 @@
+const sml = window.matchMedia("(max-width:768px)");
+console.log(sml);
+
 const navToggle = (function iife() {
   // These are available to the scopes below it but not above it
   const sidebar = document.getElementById("mySidebar");
   const main = document.getElementById("main");
   let isOpen = false;
   function toggle(targets=[sidebar,main], status=isOpen) {
-    if(!status) {
-      targets[0].style.width = "250px";
-      targets[1].style.marginLeft = "250px";
+    if (sml) {
+      if(!status) {
+        targets[0].style.width = "100px";
+      } else {
+        targets[0].style.width = "0px";
+      }
     } else {
-      targets[0].style.width = "0";
-      targets[1].style.marginLeft = "0";
-    }
-    return !status
+      if(!status) {
+        targets[0].style.width = "250px";
+        targets[1].style.marginLeft = "250px";
+      } else {
+        targets[0].style.width = "0";
+        targets[1].style.marginLeft = "0";
+      }
+    };
+    return !status;
   }
   // this marks the end of what we don't want to be called over and over again
   // when we use those they aren't going to be redeclarations of themselves
